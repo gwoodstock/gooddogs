1df1a584bfe9ffc1967e682586d75b499819c2ec
Gene Woodstock <99146612+gwoodstock@users.noreply.github.com>
2022-02-16 14:50:53 -0800

    Gene (#89)
    * rfc predictor updated

--- models/predict.py
+++ models/predict.py
@@ -1,13 +1,30 @@
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
-def predict_dog(trained_classifer, input_features):
+
+def predict_dog(input_features, trained_classifer, transformer):
     """
     predict doggo adoption
 
+    :param input_features: 2D numpy array containing dog age (as class, not numeric), breed, and condition. Formatted in .lower()
     :param trained_classifer: sklearn estimator class
-    :param input_features: numpy array like (4 numeric values)
+    :param transformer: sklearn one hote encoder transformer
     :return: numpy array of string dtype (the model's prediction)
 
+    example input: np.array([['senior', 'dachshund', 'aged'], ['adult', 'dachshund', 'normal'], ['baby', 'dachshund', 'injured']])
     """
 
-    return trained_classifer.predict(input_features.reshape(1, -1))
+    def encode(inputs, transformer):
+        """
+        Transform list of raw categorical inputs into sparse matrix for modeling.
+        :param inputs: ordered list containing dog age (as class, not numeric), breed, and condition
+        :param transformer: sklearn one hote encoder transformer
+        :return: sparse matrix of model's inputs for prediction
+        """
+        return transformer.transform(np.array(inputs).reshape(1, -1))
+
+    data = encode(inputs=input_features, transformer=transformer)
+    prediction = trained_classifer.predict_proba(data)
+
+    return prediction[:,1]
+

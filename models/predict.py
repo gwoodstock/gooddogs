0807d264a5550995d8931d3e801de115207c030d
gwoodstock <gene@genewoodstock.com>
2022-02-16 14:44:38 -0800

    Revert "rfc predictor"
    This reverts commit 7a498f18bd4d3af5f9e42fcaf98feb51b90eed18.

--- models/predict.py
+++ models/predict.py
@@ -1,30 +1,13 @@
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
 
-
-def predict_dog(input_features, trained_classifer, transformer):
+def predict_dog(trained_classifer, input_features):
     """
     predict doggo adoption
 
-    :param input_features: 2D numpy array containing dog age (as class, not numeric), breed, and condition. Formatted in .lower()
     :param trained_classifer: sklearn estimator class
-    :param transformer: sklearn one hote encoder transformer
+    :param input_features: numpy array like (4 numeric values)
     :return: numpy array of string dtype (the model's prediction)
 
-    example input: np.array([['senior', 'dachshund', 'aged'], ['adult', 'dachshund', 'normal'], ['baby', 'dachshund', 'injured']])
     """
 
-    def encode(inputs, transformer):
-        """
-        Transform list of raw categorical inputs into sparse matrix for modeling.
-        :param inputs: ordered list containing dog age (as class, not numeric), breed, and condition
-        :param transformer: sklearn one hote encoder transformer
-        :return: sparse matrix of model's inputs for prediction
-        """
-        return transformer.transform(np.array(inputs).reshape(1, -1))
-
-    data = encode(inputs=input_features, transformer=transformer)
-    prediction = trained_classifer.predict_proba(data)
-
-    return prediction[:,1]
-
+    return trained_classifer.predict(input_features.reshape(1, -1))

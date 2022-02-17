5c8f9fd3702876a3d7ab194a3268983a5b5063f1
ncox0906 <ncox0906@gmail.com>
2022-02-17 13:53:02 -0800

    streamlit work

--- streamlit/pages/available_dogs_nate.py
+++ streamlit/pages/available_dogs_nate.py
@@ -4,6 +4,7 @@ import streamlit as st
 import numpy as np
 import pickle
 from predict import predict_dog
+from clean_breeds import breed_func
 
 
 def app():
@@ -14,8 +15,8 @@ def app():
     # imports 
     import petpy
     import pandas as pd
-    import pyarrow.parquet as pq
-    import pyarrow as pa
+    # import pyarrow.parquet as pq  # are these used?
+    # import pyarrow as pa
 
     # import model from production_model.py
 
@@ -36,6 +37,9 @@ def app():
     # user input: enter location for search
     zipcode = st.text_input('Enter your zipcode')
 
+    # initialize dogs
+    dogs = []
+
     # when button is pushed: search for dogs in zipcode entered (automatically searches for dogs most recently added)
     if st.button('Search for Dogs'):
         try:
@@ -48,55 +52,70 @@ def app():
                     location=f'{zipcode}',
                     status='adoptable'
                 )
-            # st.write(type(dogs))
 
         # if no dogs found in zipcode entered:
         except:
             st.header('No dogs found in your area')
             
-        
-            
+        # if dogs were found, run this block of code
+        # makes predictions and displays dogs
         if len(dogs) > 0:
 
+            # copy dogs dataframe and selects only the columns that will be used
             dogs_copy = dogs.copy()
-            dogs_copy = dogs_copy[['age', 'breeds.primary', 'photos', 'name', 'size', 'gender', 'url']]
+            dogs_copy = dogs_copy[['age', 'breeds.primary', 'photos', 'name', 'size', 'gender', 'url', 'attributes.special_needs']]
+
+            # maps special needs to be used in the model
+            dogs_copy['attributes.special_needs'] = dogs_copy['attributes.special_needs'].map({True: 0, False: 1})
+
+            # create prediction column
             dogs_copy['pred'] = np.nan
 
+            # prediction section
+            # loops over all the dogs in the search results
             for i in range(len(dogs_copy)):
+                breed = dogs_copy.iloc[i]['breeds.primary'].lower()
+                cleaned_breed = breed_func(breed)
 
+                # creates an np.array of the features to be used in the prediction
                 np_dog = np.array(
                     [
                         dogs_copy.iloc[i]['age'].lower(), 
-                        dogs_copy.iloc[i]['breeds.primary'].lower(),
-                        'normal'
+                        cleaned_breed,
+                        dogs_copy.iloc[i]['attributes.special_needs']
                     ]
                 )
 
+                # makes the prediction for this dog and assigns the score to the pred column
                 prediction = np.round(predict_dog(np_dog, classifier, transformer) * 100, 2)
                 dogs_copy.loc[i, 'pred'] = prediction
 
+            # sort dataframe so the highest % are at the top
             dogs_copy.sort_values(by='pred', ascending=False, inplace=True)
+
         
             # output: feed of available dogs determined to be 'high risk' by model
             st.header('Furry friends available in your area:')
             for i in range(len(dogs_copy)):
                 
                 try:
-
+                    # creates columns for spacers and displaying the image and info
                     col1, col2, col3, col4 = st.columns([1,3,3,1])
                     
                     with col2:
-                        # - photo
+                        # photo
                         if dogs_copy.iloc[i]['photos'] != []:
                             st.image(dogs_copy.iloc[i]['photos'][0]['medium'])
                         else:
+                            # default image
                             st.image(
                                 image='https://cdn.pixabay.com/photo/2016/04/03/21/54/dog-1305702_960_720.png',
                                 caption='Photo Not Available',
                                 # width=300
                             )
+
                     with col3:
-                        # - dog's name and details
+                        # dog's name and details
                         st.subheader(dogs_copy.iloc[i]['name'])
                         st.metric('At Risk %', dogs_copy.iloc[i]['pred'])
                         st.write('Breed: ', dogs_copy.iloc[i]['breeds.primary'])
@@ -104,7 +123,7 @@ def app():
                         st.write('Gender: ', dogs_copy.iloc[i]['gender'])
                         st.write('Age: ', dogs_copy.iloc[i]['age'])
                         
-                        # - link to petfinder
+                        # link to petfinder
                         url = dogs_copy.iloc[i]['url']
                         st.write("[Learn More on Petfinder!](%s)" % url) 
                 except:
@@ -115,4 +134,5 @@ def app():
                 st.write(":heavy_minus_sign:" * 30)
                 st.text("")
 
-        
+    # clears dogs to be able to do another search   
+    dogs = []
